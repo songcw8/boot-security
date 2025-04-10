10cae3a42fc7ce2a90e19679d4203bf10ba0899c
songcw8 <ben990158@gmail.com>
2025-04-10 10:51:28 +0900

    firstCommit

--- src/main/java/org/example/bootsecurity/controller/MyController.java
+++ src/main/java/org/example/bootsecurity/controller/MyController.java
@@ -1,4 +1,12 @@
 package org.example.bootsecurity.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MyController {
+    @GetMapping
+    public String index() {
+        return "index";
+    }
 }
