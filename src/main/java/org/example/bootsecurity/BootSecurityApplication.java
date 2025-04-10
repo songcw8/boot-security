6befc851fca22bdff837b0776568423c2783ec24
songcw8 <ben990158@gmail.com>
2025-04-10 16:43:03 +0900

    feat:TimeZone-seoul

--- src/main/java/org/example/bootsecurity/BootSecurityApplication.java
+++ src/main/java/org/example/bootsecurity/BootSecurityApplication.java
@@ -3,10 +3,13 @@ package org.example.bootsecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BootSecurityApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(BootSecurityApplication.class, args);
     }
 
